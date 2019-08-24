faea532c5dca0dab2845231c75212b485171b782
Enea Barbetta <enea.barbetta@gmail.com>
2019-08-24 14:57:45 +0200

    Added CORS

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,3 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 Route::post('api/send', 'Otrigg\Formmailer\Http\Controllers\FormmailerController@send')->middleware('checkform');
